cf977bb66563c1fc515db66286c58a899e5de871
Andreas von Burg <a@von-burg.net>
2017-04-10 11:24:45 +0200

    Update rocket.ts

--- src/public/assets/rocket/ts/rocket.ts
+++ src/public/assets/rocket/ts/rocket.ts
@@ -1,10 +1,14 @@
 namespace rocket {
 	jQuery(document).ready(function ($) {
-		var container = new rocket.cmd.Container($("#rocket-content-container"));
+		var jqContainer = $("#rocket-content-container");
+		var container = new rocket.cmd.Container(jqContainer);
 		var monitor: rocket.cmd.Monitor = new rocket.cmd.Monitor(container);
 
 		monitor.scanMain($("#rocket-global-nav"), container.getMainLayer());
-		
+		monitor.scan(jqContainer);
+		n2n.dispatch.registerCallback(function () {
+			monitor.scan(jqContainer);
+		});
 		
 		(function () {
 			$(".rocket-impl-overview").each(function () {
@@ -19,10 +23,9 @@ namespace rocket {
 		}) ();
 	});
 	
-	
-	class Rocket {
-	}
-	
-	export function entry(jqElem: JQuery) {
+	export function handleErrorResponse(responseObject) {
+		alert(JSON.stringify(responseObject));
+		
+		$("html").html(responseObject.responseText);
 	}
 }
