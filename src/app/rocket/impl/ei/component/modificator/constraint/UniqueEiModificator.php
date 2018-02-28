a5081096b9f44d555f87c0c24deefd0e33968072
Andreas von Burg <a@von-burg.net>
2018-02-28 23:26:24 +0100

    UniqueEiModificator bug fixes

--- src/app/rocket/impl/ei/component/modificator/constraint/UniqueEiModificator.php
+++ src/app/rocket/impl/ei/component/modificator/constraint/UniqueEiModificator.php
@@ -3,16 +3,6 @@ namespace rocket\impl\ei\component\modificator\constraint;
 
 use rocket\spec\ei\manage\util\model\Eiu;
 use rocket\spec\ei\component\EiConfigurator;
-use rocket\impl\ei\component\EiConfiguratorAdapter;
-use n2n\core\container\N2nContext;
-use n2n\web\dispatch\mag\MagCollection;
-use n2n\impl\web\dispatch\mag\model\MultiSelectMag;
-use n2n\util\config\LenientAttributeReader;
-use n2n\impl\web\dispatch\mag\model\MagForm;
-use n2n\web\dispatch\mag\MagDispatchable;
-use n2n\util\config\Attributes;
-use rocket\spec\ei\component\EiSetupProcess;
-use n2n\reflection\CastUtils;
 use rocket\spec\ei\manage\critmod\CriteriaConstraint;
 use n2n\l10n\MessageCode;
 use n2n\persistence\orm\criteria\Criteria;
@@ -55,10 +45,18 @@ class UniqueEiModificator extends IndependentEiModificatorAdapter {
 		$this->uniquePerEiPropPaths = $uniquePerEiPropPaths;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * @see \rocket\impl\ei\component\modificator\adapter\IndependentEiModificatorAdapter::createEiConfigurator()
+	 */
 	function createEiConfigurator(): EiConfigurator {
 		return new UniqueEiConfigurator($this);		
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 * @see \rocket\impl\ei\component\modificator\adapter\IndependentEiModificatorAdapter::setupEiEntry()
+	 */
 	function setupEiEntry(Eiu $eiu) {
 		if ($eiu->entry()->isDraft() 
 				|| (empty($this->uniqueEiPropPaths) && empty($this->uniquePerEiPropPaths))) {
@@ -69,7 +67,10 @@ class UniqueEiModificator extends IndependentEiModificatorAdapter {
 			$this->validate($eiu);
 		});
 	}
-		
+	
+	/**
+	 * @param Eiu $eiu
+	 */
 	private function validate(Eiu $eiu) {
 		$eiuEntry = $eiu->entry();
 		$eiuEngine = $eiu->engine();
@@ -107,61 +108,5 @@ class UniqueEiModificator extends IndependentEiModificatorAdapter {
 			
 			$criteria->where()->match($ci, '=', $cv);
 		}
-	}
-}
-
-class UniqueEiConfigurator extends EiConfiguratorAdapter {
-	const ATTR_UNIQUE_PROPS_KEY = 'uniqueProps';
-	const ATTR_UNIQUE_PER_PROPS_KEY = 'uniquePerProps';
-	
-	function createMagDispatchable(N2nContext $n2nContext): MagDispatchable {
-		$lar = new LenientAttributeReader($this->attributes);
-		
-		$eiu = $this->eiu($n2nContext);
-		$options = $eiu->engine()->getGenericEiPropertyOptions();
-		
-		$magCollection = new MagCollection();
-		$magCollection->addMag(self::ATTR_UNIQUE_PROPS_KEY, 
-				new MultiSelectMag($label, $options, $lar->getScalarArray(self::ATTR_UNIQUE_PROPS_KEY)));
-		
-		$magCollection->addMag(self::ATTR_UNIQUE_PER_PROPS_KEY,
-				new MultiSelectMag($label, $options, $lar->getScalarArray(self::ATTR_UNIQUE_PER_PROPS_KEY)));
-		
-		return new MagForm($magCollection);
-	}
-	
-	function saveMagDispatchable(MagDispatchable $magDispatchable, N2nContext $n2nContext) {
-		$this->attributes = new Attributes();
-		$this->attributes->set(self::ATTR_UNIQUE_PROPS_KEY,
-				$magDispatchable->getPropertyValue(SELF::ATTR_UNIQUE_PROPS_KEY));
-		$this->attributes->set(self::ATTR_UNIQUE_PER_PROPS_KEY,
-				$magDispatchable->getPropertyValue(SELF::ATTR_UNIQUE_PROPS_KEY));
-	}
-	
-	function setup(EiSetupProcess $eiSetupProcess) {
-		$uniqueEiModificator = $this->eiComponent;
-		CastUtils::assertTrue($uniqueEiModificator instanceof UniqueEiModificator);
-		
-		$eiuEngine = $eiSetupProcess->eiu()->engine();
-		
-		$uniqueEiPropPaths = array();
-		foreach ($this->attributes->getScalarArray(self::ATTR_UNIQUE_PROPS_KEY, false) as $eiPropPathStr) {
-			$eiPropPath = EiPropPath::create($eiPropPathStr);
-			
-			if ($eiuEngine->containsGenericEiProperty($eiPropPath)) {
-				$uniqueEiPropPaths[] = $eiPropPath;
-			}
-		}
-		$uniqueEiModificator->setUniqueEiPropPaths($uniqueEiPropPaths);
-		
-		$uniquePerEiPropPaths = array();
-		foreach ($this->attributes->getScalarArray(self::ATTR_UNIQUE_PER_PROPS_KEY, false) as $eiPropPathStr) {
-			$eiPropPath = EiPropPath::create($eiPropPathStr);
-			
-			if ($eiuEngine->containsGenericEiProperty($eiPropPath)) {
-				$uniquePerEiPropPaths[] = $eiPropPath;
-			}
-		}
-		$uniqueEiModificator->setUniquePerEiPropPaths($uniquePerEiPropPaths);
 	}
 }
