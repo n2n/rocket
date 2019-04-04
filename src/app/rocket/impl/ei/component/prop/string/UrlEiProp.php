c6f96b87f8ae3a3a915552541df5da996151bcd6
Andreas von Burg <a@von-burg.net>
2019-04-04 18:34:41 +0200

    same as before bug without bugs

--- src/app/rocket/impl/ei/component/prop/string/UrlEiProp.php
+++ src/app/rocket/impl/ei/component/prop/string/UrlEiProp.php
@@ -59,7 +59,7 @@ class UrlEiProp extends AlphanumericEiProp {
 		parent::setEntityProperty($entityProperty);
 	}
 	
-	public function buildQuickSearchProp(Eiu $eiu): QuickSearchableEiProp {
+	public function buildQuickSearchProp(Eiu $eiu): ?QuickSearchableEiProp {
 		if ($this->entityProperty instanceof UrlEntityProperty) {
 			return null;
 		}
