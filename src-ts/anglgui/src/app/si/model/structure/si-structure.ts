8f0701ae4f3ac7d34056300fdac29c983d999147
Andreas von Burg <a@von-burg.net>
2019-04-26 18:49:49 +0200

    dev

---
+++ src-ts/anglgui/src/app/si/model/structure/si-structure.ts
@@ -0,0 +1,8 @@
+
+import { SiEntry } from "src/app/si/model/content/si-entry";
+import { SiStructureType } from "src/app/si/model/structure/si-field-structure-declaration";
+
+export class SiStructure {
+	entry: SiEntry|null;
+	type: SiStructureType|null;
+}
--- src-ts/anglgui/src/app/ui/structure/comp/zone/zone-content.directive.ts
+++ src-ts/anglgui/src/app/ui/structure/comp/zone/zone-content.directive.ts
@@ -1,12 +1,15 @@
 import { Directive } from '@angular/core';
-import { ViewContainerRef } from "@angular/core";
+import { ViewContainerRef, Input } from "@angular/core";
 
 @Directive({
   selector: '[rocketZoneContent]'
 })
 export class ZoneContentDirective {
 
-  constructor(public viewContainerRef: ViewContainerRef) { 
-  }
+	
+	constructor(public viewContainerRef: ViewContainerRef) { 
+	}
+	
+	
 
 }
