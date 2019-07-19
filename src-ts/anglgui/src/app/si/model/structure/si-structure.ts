d3068a90eb3eb21fd14fc714f0ffbb5d745d176f
Andreas von Burg <a@von-burg.net>
2019-07-19 21:54:24 +0200

    dev

--- src-ts/anglgui/src/app/si/model/structure/si-structure.ts
+++ src-ts/anglgui/src/app/si/model/structure/si-structure.ts
@@ -32,7 +32,7 @@ export class SiStructure {
 		
 		if (this.model) {
 			errors.push(...this.model.getZoneErrors());
-		
+			console.log(this.model)
 			for (const child of this.model.getChildren()) {
 				errors.push(...child.getZoneErrors());
 			}
