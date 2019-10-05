325f1325fde4907a2f9e61e2d1b734c08480f0ee
andy <andy@FLUSSPFERDKROKODIL-PC>
2019-10-05 20:50:13 +0200

    dev

--- src-ts/anglgui/src/app/si/model/meta/si-type.ts
+++ src-ts/anglgui/src/app/si/model/meta/si-type.ts
@@ -1,4 +1,6 @@
 
+import { SiTypeQualifier } from "src/app/si/model/meta/si-type-qualifier";
+
 export class SiType {
 	constructor(public qualifier: SiTypeQualifier, public props: Map<string, SiProp>) {
 		
