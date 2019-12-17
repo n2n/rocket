26efa50511936518181c22af1ea061d5e5fa766a
Andreas von Burg <a@von-burg.net>
2019-12-17 14:59:58 +0100

    dev

--- src-ts/anglgui/src/app/si/model/content/impl/split/comp/split/split.component.ts
+++ src-ts/anglgui/src/app/si/model/content/impl/split/comp/split/split.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SplitViewStateService } from '../../model/state/split-view-state.service';
 import { SplitModel } from '../split-model';
 import { SplitViewStateSubscription } from '../../model/state/split-view-state-subscription';
+import { UiStructure } from 'src/app/ui/structure/model/ui-structure';
 
 @Component({
 	selector: 'rocket-split',
@@ -11,18 +12,26 @@ import { SplitViewStateSubscription } from '../../model/state/split-view-state-s
 export class SplitComponent implements OnInit {
 
     model: SplitModel;
+    uiStructure: UiStructure;
 
-    private subscription: SplitViewStateSubscription;
+	private subscription: SplitViewStateSubscription;
 
 	constructor(private viewStateService: SplitViewStateService) {
 	}
 
 	ngOnInit() {
         this.subscription = this.viewStateService.subscribe(this.model.getSplitOptions());
+        
+
+
+	}
+
+	ngOnDestroy() {
+		this.subscription.cancel();
     }
     
-    ngOnDestroy() {
-        this.subscription.cancel();
+    sfd() {
+        this.subscription;
     }
 
 }
--- src-ts/anglgui/src/app/si/model/content/impl/split/model/split-si-field.ts
+++ src-ts/anglgui/src/app/si/model/content/impl/split/model/split-si-field.ts
@@ -1,10 +1,17 @@
 import { SiField } from '../../../si-field';
 import { UiContent } from 'src/app/ui/structure/model/ui-content';
 import { SiFieldAdapter } from '../../common/model/si-field-adapter';
+import { SplitModel } from '../comp/split-model';
+import { TypeUiContent } from 'src/app/ui/structure/model/impl/type-si-content';
+import { SplitOption } from './split-option';
+import { Observable } from 'rxjs';
+import { SplitContext } from './split-context';
+import { SiEntry } from '../../../si-entry';
+import { map } from 'rxjs/operators';
+import { SplitComponent } from '../comp/split/split.component';
 
-export class SplitSiField extends SiFieldAdapter {
-
-	contextSiField: SiField|null;
+export class SplitSiField extends SiFieldAdapter implements SplitModel {
+	splitContext: SplitContext|null;
 
 	constructor(public relFieldId: string) {
 		super();
@@ -23,6 +30,27 @@ export class SplitSiField extends SiFieldAdapter {
 	}
 
 	protected createUiContent(): UiContent {
-		throw new Error('Method not implemented.');
+		return new TypeUiContent(SplitComponent, (ref, uiStructure) => {
+			ref.instance.model = this;
+			ref.instance.uiStructure = uiStructure;
+		});
+	}
+
+	getSplitOptions(): SplitOption[] {
+		if (this.splitContext) {
+			return this.splitContext.getSplitOptions();
+		}
+
+		return [];
+	}
+
+	getSiField$(key: string): Observable<SiField> {
+		if (!this.splitContext) {
+			throw new Error('Method not implemented.');
+		}
+
+		return this.splitContext.getEntry$(key).pipe(map((entry: SiEntry) => {
+			return entry.selectedEntryBuildup.getFieldById(this.relFieldId);
+		}));
 	}
 }
