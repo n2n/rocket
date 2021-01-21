24a4dacec685096b1543aaa4468386b7a1a49994
nikolai <nikolai@schmid.guru>
2021-01-21 14:52:10 +0100

    iframe eiprop update

---
+++ src-ts/anglgui/src/app/ui/util/comp/url-iframe/url-iframe.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UrlIframeComponent } from './url-iframe.component';
+
+describe('UrlIframeComponentComponent', () => {
+  let component: UrlIframeComponent;
+  let fixture: ComponentFixture<UrlIframeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UrlIframeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UrlIframeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
