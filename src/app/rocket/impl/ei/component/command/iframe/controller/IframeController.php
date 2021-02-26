7dce36692cba16c02f585f9b00b4f29d8512cb95
nikolai <nikolai@schmid.guru>
2021-02-26 15:54:12 -0300

    added iframe pid checks

--- src/app/rocket/impl/ei/component/command/iframe/controller/IframeController.php
+++ src/app/rocket/impl/ei/component/command/iframe/controller/IframeController.php
@@ -2,6 +2,7 @@
 namespace rocket\impl\ei\component\command\iframe\controller;
 
 use n2n\web\http\controller\ControllerAdapter;
+use n2n\web\http\PageNotFoundException;
 use n2n\web\ui\Raw;
 use rocket\ei\util\EiuCtrl;
 use rocket\impl\ei\component\command\iframe\config\IframeConfig;
@@ -24,13 +25,9 @@ class IframeController extends ControllerAdapter {
 	}
 
 	function index(int $pid = null) {
-		// test entry command and no pid = pagenotfound
-		// if general command and pid = pagenotfound
-		// if entrycommand & pid = verify pid exists eiuCtrl->lookupObject(pid)
-
 		$eiuCtrl = EiuCtrl::from($this->cu());
 
-		$eiuCtrl->lookupObject($pid);
+		$this->verifyPid($pid);
 
 		if (null !== ($url = $this->iframeConfig->getUrl())) {
 			$url = $url->ext($pid);
@@ -61,6 +58,19 @@ class IframeController extends ControllerAdapter {
 		
  		$this->delegate($controller);
  	}
-	
-	
+
+	private function verifyPid(?int $pid) {
+		if ($this->iframeConfig->isEntryCommand()) {
+			if ($pid !== null) {
+				// throws PageNotFound if pid invalid
+				EiuCtrl::from($this->cu())->lookupObject($pid);
+			} else {
+				throw new PageNotFoundException();
+			}
+		} elseif ($pid !== null) {
+			throw new PageNotFoundException();
+		}
+	}
+
+
 }
