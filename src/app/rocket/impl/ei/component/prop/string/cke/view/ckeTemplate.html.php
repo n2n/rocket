984c51995a0b6ad0c5478977a975c6e372062fb3
Andreas von Burg <a@von-burg.net>
2021-12-14 11:35:50 +0100

    csp

--- src/app/rocket/impl/ei/component/prop/string/cke/view/ckeTemplate.html.php
+++ src/app/rocket/impl/ei/component/prop/string/cke/view/ckeTemplate.html.php
@@ -31,6 +31,7 @@
 
     $html->meta()->addLibrary(new JQueryLibrary(3));
     $html->meta()->addLibrary(new CkeLibrary());
+	$html->meta()->bodyStart()->addJs('js/cke-init.js');
 
     $attrs = array('name' => 'content');
 ?>
@@ -47,56 +48,6 @@
                 height: 100% !important;
             }
         </style>
-
-        <script>
-            CKEDITOR.config.removeButtons = 'Maximize'; // maximize causes cke height = 0. Needs to be fixed
-
-            CKEDITOR.on('instanceReady', function(e) {
-                var cke = CKEDITOR.instances['content'];
-                cke.on('change', function() {
-                    cke.updateElement();
-                    var element = cke.element.$;
-                    if ('createEvent' in document) {
-                        var event = document.createEvent('HTMLEvents');
-                        event.initEvent('change', false, true);
-                        element.dispatchEvent(event);
-                    } else {
-                        element.fireEvent('onchange');
-                    }
-                });
-
-                // CKEDITOR.on('dialogDefinition', function (e) {
-                //     var dialog = e.data.definition.dialog;
-                //     dialog.on('show', function () {
-                //         iframeJq.css('height', this.getSize().height + 50);
-                //     });
-                //     dialog.on('hide', function () {
-                //         iframeJq.css('height', initialHeight);
-                //     });
-                // });
-
-                var focusHeightIncreasePx = 300;
-
-                var initialEditorHeight;
-                var initialEditorIframeHeight;
-                e.editor.on('focus', function(event) {
-                    var editorJq = $(this.element.$.parentElement.parentElement);
-                    var editorIframeJq = $(this.element.$.parentElement).find('iframe');
-
-                    initialEditorHeight = editorJq.height();
-                    initialEditorIframeHeight = editorIframeJq.height();
-
-                    editorJq.css('height', (initialEditorHeight + focusHeightIncreasePx));
-                    editorIframeJq.height((initialEditorIframeHeight + focusHeightIncreasePx));
-                });
-
-                e.editor.on('blur', function(event) {
-                    $(this.element.$.parentElement.parentElement).css('height', initialEditorHeight);
-                    $(this.element.$.parentElement).find('iframe').height(initialEditorIframeHeight);
-                });
-
-            });
-        </script>
 
         <?php $html->out($ckeHtml->getTextarea('', $ckeComposer, $config->getCkeCssConfig(),
             $config->getCkeLinkProviders()->getArrayCopy(), $attrs)) ?>
