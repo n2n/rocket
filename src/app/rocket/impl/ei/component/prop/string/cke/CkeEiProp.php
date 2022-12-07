38f852ec5a6d87829f091e34c5cae110675b17ca
andreasvonburg <a@von-burg.net>
2022-12-07 11:25:17 +0100

    deprecated param fix

--- src/app/rocket/impl/ei/component/prop/string/cke/CkeEiProp.php
+++ src/app/rocket/impl/ei/component/prop/string/cke/CkeEiProp.php
@@ -62,7 +62,7 @@ class CkeEiProp extends AlphanumericEiProp {
 	    
 		if ($eiu->guiFrame()->isCompact()) {
 			return $eiu->factory()->newGuiField(
-					SiFields::stringOut(StringUtils::reduce(html_entity_decode(strip_tags($value), null, N2N::CHARSET), 50, '...')));
+					SiFields::stringOut(StringUtils::reduce(html_entity_decode(strip_tags($value), encoding: N2N::CHARSET), 50, '...')));
 		}
 
 		return $eiu->factory()->newGuiField(SiFields::stringOut((string) $value));
