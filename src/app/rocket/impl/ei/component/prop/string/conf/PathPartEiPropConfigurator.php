bb1850a04599ce6045c8d699cff7266684a6478a
Thomas Guenther <guenther@hnm.ch>
2024-07-12 13:16:20 +0200

    Added \n2n\spec\dbo\meta\structure\Column compatibility - bug fix

--- src/app/rocket/impl/ei/component/prop/string/conf/PathPartEiPropConfigurator.php
+++ src/app/rocket/impl/ei/component/prop/string/conf/PathPartEiPropConfigurator.php
@@ -72,7 +72,7 @@ class PathPartEiPropConfigurator extends AlphanumericEiPropConfigurator {
 	}
 	
 	
-	public function initAutoEiPropAttributes(N2nContext $n2nContext, ColumnPathPartEiPropConfigurator $column = null) {
+	public function initAutoEiPropAttributes(N2nContext $n2nContext, Column|\n2n\spec\dbo\meta\structure\Column $column = null) {
 		parent::initAutoEiPropAttributes($n2nContext, $column);
 	
 		$options = $this->getBaseEiPropIdOptions();
